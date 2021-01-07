4f992488fe26b60f22274bc1783836e3b6a6804f
sheng <shengyefe@gmail.com>
2021-01-07 23:07:03 +0100

    first

---
+++ src/Programacion/Sort.java
@@ -0,0 +1,54 @@
+package Programacion;
+
+import java.util.Arrays;
+
+/**
+ * Programacion
+ * Nombre_project: Ejercicios
+ * Array
+ * Created by: sheng
+ * Date : 18/12/2020
+ * Description:
+ **/
+
+    public class Sort {
+
+        public static void main(String[] args) {
+
+            int[]numeros = {2,8,56,3,4,8,96,21,42,56,75,11,
+
+                    52,43,62,112,14,3,9,58,27,91,64,38};
+
+
+
+            System.out.println("SIN ORDENAR");
+
+            //El siguiente bucle for imprimirá los números
+
+            //tal cual fueron introducidos
+
+            for(int x = 1; x < numeros.length; x++){
+
+                System.out.println(x+ ": " + numeros[x]);
+
+            }
+
+            System.out.println("ORDENADOS");
+
+            //Llamada al método sort de la clase Arrays para
+
+            //ordenar el array
+
+            Arrays.sort(numeros);
+
+            //Muestra el array ordenadado ascendentemente
+
+            for(int i = 1; i < numeros.length; i++){
+
+                System.out.println(i+ ": " + numeros[i]);
+
+            }
+
+        }
+
+    }
